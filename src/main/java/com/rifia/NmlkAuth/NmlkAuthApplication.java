1e721a08694ebd346df6d9ed67f457e61298e9cc
Marina Filina <45katerain@gmail.com>
2020-07-15 14:19:16 +0300

    Initial commit

---
+++ src/main/java/com/rifia/NmlkAuth/NmlkAuthApplication.java
@@ -0,0 +1,13 @@
+package com.rifia.NmlkAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmlkAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmlkAuthApplication.class, args);
+	}
+
+}
